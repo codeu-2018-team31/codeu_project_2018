ce7d6dc2d1e0de86dac2f75de11aee237547b61e
Ransford Antwi <ransford@seas.upenn.edu>
2018-05-24 22:59:27 +0200

    Fixed conflict in tagtest

--- src/test/java/codeu/model/data/TagTest.java
+++ src/test/java/codeu/model/data/TagTest.java
@@ -15,14 +15,9 @@ public class TagTest {
     Instant creation = Instant.now();
     Conversation conversation = new Conversation(UUID.randomUUID(), UUID.randomUUID(), "test", creation);
 
-<<<<<<< HEAD
-    Tag test_tag = new Tag(id, conv_id, tag, creation);
-=======
     Tag test_tag = new Tag(id, conversation, tag, creation);
->>>>>>> 8eff34afbfa5875fec026b56da002eac05818858
-
     Assert.assertEquals(id, test_tag.getId());
-    Assert.assertEquals(conv_id, test_tag.getConversationId());
+    Assert.assertEquals(conv_id, test_tag.getConversation().getId();
     Assert.assertEquals(tag, test_tag.getTag());
     Assert.assertEquals(creation, test_tag.getCreationTime());
   }
